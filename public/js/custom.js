1bd665a9ce9121a8e5672c478e271c9754626fc4
NCC-17039 <120711536+Bashir-1@users.noreply.github.com>
2023-12-24 20:40:38 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,15 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
+
+
+// 查找头部
+var head = document.head || document.getElementsByTagName('head')[0];
+
+// 创建meta标签
+var meta = document.createElement('meta');
+
+// 设置meta标签的属性
+meta.name = 'referrer';
+meta.content = 'never';
+
+// 将meta标签添加到头部
+head.appendChild(meta);
